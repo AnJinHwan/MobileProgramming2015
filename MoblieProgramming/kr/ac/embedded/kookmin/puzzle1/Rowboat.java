e3cefc5b7b96a5d34da56d9e2b6b82e935424882
User <User@192.168.10.53>
2015-10-07 00:35:47 +0900

    puzzle1

---
+++ MoblieProgramming/kr/ac/embedded/kookmin/puzzle1/Rowboat.java
@@ -0,0 +1,11 @@
+package kr.ac.embedded.kookmin.puzzle1;
+
+public class Rowboat extends Boat
+{
+	
+	public void rowTheBoat() 
+	{
+		System.out.print("stroke natasha");
+	}
+	
+}
---
+++ MoblieProgramming/kr/ac/embedded/kookmin/puzzle1/Sailboat.java
@@ -0,0 +1,10 @@
+package kr.ac.embedded.kookmin.puzzle1;
+
+public class Sailboat extends Boat
+{
+	public void move()
+	{
+		System.out.print("hoist sail ");
+	}
+	
+}
